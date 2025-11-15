dcaeba429ae6f1c6184bfc350a730497124e021f
ssagastume11 <sagassergio123@gmail.com>
2025-11-15 15:04:44 -0500

    BigQuery SQL queries

--- sql/Step 2_ Prepare.sql
+++ sql/Step 2_ Prepare.sql
@@ -1,1 +1,9 @@
+-- Objective:
+-- The goal of this step is to load, clean, and organize the Cats vs. Dogs dataset to ensure it is ready for accurate and efficient analysis. This includes validating data integrity, and handling missing values, converting data types, and preparing it for comparing cat and dog ownership trends in U.S. households.
 
+-- Key Actions:
+-- * Ensure correct data types for numeric and categorical columns.
+-- * Handle null or missing values.
+-- * Remove unnecessary index columns (e.g., int64_field_0).
+-- * Rename columns for clarity and readability.
+-- * Create a clean version of the dataset for analysis.
